f1e1ff330f84ee8006c52065789e3e31a38d8fb0
Daniel Winsvold <danielwinsvold@gmail.com>
2020-09-14 08:47:55 +0200

    importerer farger direkte fra designsystemet som nå tilbyr navfarger som js-objekt

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,6 @@
 import { css, keyframes } from 'styled-components/macro';
 import { pxToRem } from './utils';
+import navFarger from 'nav-frontend-core';
 
 export const fadeIn = keyframes`
     from {
@@ -8,39 +9,11 @@ export const fadeIn = keyframes`
     }
 `;
 
-const colors = {
-  navRod: '#C30000',
-  navOransje: '#FF9100',
-  navLimeGronn: '#A2AD00',
-  navGronn: '#06893A',
-  navLilla: '#634689',
-  navDypBla: '#005B82',
-  navBla: '#0067C5',
-  navLysBla: '#66CBEC',
-  navMorkGra: '#3E3832',
-  navGra80: '#59514B',
-  navGra60: '#78706A',
-  navGra40: '#B7B1A9',
-  navGra20: '#C6C2BF',
-  navLysGra: '#E9E7E7',
-  orangeFocus: '#FFBD66',
-  redError: '#BA3A26',
-  white: '#FFF',
-  pinkErrorBg: '#F3E3E3',
-  navBlaLighten80: '#CCE1F3',
-  navBlaLighten60: '#99C2E8',
-  navDypBlaLighten80: '#CCDEE6',
-  navLysBlaLighten80: '#E0F5FB',
-  navGronnLighten60: '#9BD0B0',
-  navLimeGronnLighten80: '#ECEFCC',
-  fokusFarge: '#254b6d',
-};
-
 export const theme = {
   colors: {
-    ...colors,
-    bakgrunn: colors.navLysGra,
-    lenke: colors.navBla,
+    ...navFarger,
+    bakgrunn: navFarger.navLysGra,
+    lenke: navFarger.navBla,
   },
   visuallyHidden: css`
     position: absolute !important;
@@ -56,7 +29,7 @@ export const theme = {
   },
   focus: css`
     outline: none;
-    box-shadow: 0 0 0 ${pxToRem(3)} ${colors.fokusFarge};
+    box-shadow: 0 0 0 ${pxToRem(3)} ${navFarger.fokusFarge};
   `,
   borderRadius: '.2rem',
   layoutMargin: '4vmin',
@@ -65,6 +38,6 @@ export const theme = {
     animation: ${fadeIn} 0.5s backwards;
   `,
   border: {
-    banner: `0.3rem solid ${colors.navBlaLighten60}`,
+    banner: `0.3rem solid ${navFarger.navBlaLighten60}`,
   },
 };
